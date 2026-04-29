423afaeec79181f3253b2b08d25f139437ad2ac0
Articodental <admin@articodental.com>
2026-04-29 15:28:36 -0500

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,4 +1,4 @@
 import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://dbqjkjtrphjqfbxuxama.supabase.co'
-const supabaseKey = 'sb_publishable_Y7iBKl6D50iTleXfge8y9g_uRRxpQN5'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRicWpranRycGhqcWZieHV4YW1hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc0ODQwMDgsImV4cCI6MjA5MzA2MDAwOH0.eBvaM46ciJUSLyzf9d-a1Z7476fxOKnf-1ferrpkesE'
 export const supabase = createClient(supabaseUrl, supabaseKey)
